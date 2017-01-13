f8fede5c69710d251a9bbf6ca59cb54737870925
Chris Marshall <chris@codegoalie.com>
2017-01-13 13:53:49 -0500

    Implement and test full interface

--- binary.go
+++ binary.go
@@ -2,6 +2,7 @@ package heap
 
 import "errors"
 
+// BinaryHeap implements a binary tree heap data structure
 type BinaryHeap struct {
 	items []int
 }
@@ -11,11 +12,13 @@ func NewBinaryHeap() BinaryHeap {
 	return BinaryHeap{}
 }
 
+// Insert adds new items to the heap
 func (b *BinaryHeap) Insert(newb int) {
 	b.items = append(b.items, newb)
 	b.rebalance()
 }
 
+// Max removes and returns the highest valued item in the heap
 func (b *BinaryHeap) Max() (int, error) {
 	if len(b.items) < 1 {
 		return 0, errors.New("Heap has no items")
@@ -26,6 +29,7 @@ func (b *BinaryHeap) Max() (int, error) {
 	return max, nil
 }
 
+// Peek returns, but does not remove, the highest valued item in the heap
 func (b BinaryHeap) Peek() (int, error) {
 	if len(b.items) < 1 {
 		return 0, errors.New("Heap has no items")
@@ -34,4 +38,23 @@ func (b BinaryHeap) Peek() (int, error) {
 }
 
 func (b *BinaryHeap) rebalance() {
+	i := len(b.items) - 1
+	for {
+		if i < 1 {
+			return
+		}
+
+		p := parentFor(i)
+		if b.items[i] > b.items[p] {
+			b.items[i], b.items[p] = b.items[p], b.items[i]
+		}
+		i = p
+	}
+}
+
+func parentFor(i int) int {
+	if i%2 == 0 {
+		return (i / 2) - 1
+	}
+	return (i - 1) / 2
 }
--- binary_test.go
+++ binary_test.go
@@ -1,8 +1,6 @@
 package heap
 
-import (
-	"testing"
-)
+import "testing"
 
 func TestEmptyMax(t *testing.T) {
 	heap := NewBinaryHeap()
@@ -16,8 +14,141 @@ func TestMaxForOne(t *testing.T) {
 	heap := NewBinaryHeap()
 	expected := 4
 	heap.Insert(expected)
+
 	max, err := heap.Max()
-	if err != nil && max != expected {
-		t.Errorf("heap.Max() == %d; expected %d; raised %s", max, expected, err)
+
+	if err != nil {
+		t.Errorf("heap.Max() raised %s; expected no error", err.Error())
+	}
+	if max != expected {
+		t.Errorf("heap.Max() == %d; expected %d", max, expected)
+	}
+}
+
+func TestMaxForTwo(t *testing.T) {
+	heap := NewBinaryHeap()
+	expected := 4
+	heap.Insert(3)
+	heap.Insert(expected)
+
+	max, err := heap.Max()
+
+	if err != nil {
+		t.Errorf("heap.Max() raised %s; expected no error", err.Error())
+	}
+	if max != expected {
+		t.Errorf("heap.Max() == %d; expected %d", max, expected)
+	}
+}
+
+func TestMaxForThree(t *testing.T) {
+	heap := NewBinaryHeap()
+	expected := 4
+	heap.Insert(3)
+	heap.Insert(1)
+	heap.Insert(expected)
+
+	max, err := heap.Max()
+
+	if err != nil {
+		t.Errorf("heap.Max() raised %s; expected no error", err.Error())
+	}
+	if max != expected {
+		t.Errorf("heap.Max() == %d; expected %d", max, expected)
+	}
+}
+
+func TextMaxForTwenty(t *testing.T) {
+	heap := NewBinaryHeap()
+	for i := 1; i < 21; i++ {
+		heap.Insert(i)
+	}
+	expected := 21
+	heap.Insert(expected)
+
+	max, err := heap.Max()
+
+	if err != nil {
+		t.Errorf("heap.Max() raised %s; expected no error", err.Error())
+	}
+	if max != expected {
+		t.Errorf("heap.Max() == %d; expected %d", max, expected)
+	}
+}
+
+func TestMaxRemoves(t *testing.T) {
+	heap := NewBinaryHeap()
+	highest := 4
+	next := 3
+	heap.Insert(2)
+	heap.Insert(next)
+	heap.Insert(highest)
+
+	max, err := heap.Max()
+
+	if err != nil {
+		t.Errorf("heap.Max() raised %s; expected no error", err.Error())
+	}
+	if max != highest {
+		t.Errorf("heap.Max() == %d; expected %d", max, highest)
+	}
+
+	nextMax, err := heap.Max()
+
+	if err != nil {
+		t.Errorf("heap.Max() raised %s; expected no error", err.Error())
+	}
+	if nextMax != next {
+		t.Errorf("heap.Max() == %d; expected %d", nextMax, next)
+	}
+}
+
+func TestPeekNoRemove(t *testing.T) {
+	heap := NewBinaryHeap()
+	highest := 4
+	heap.Insert(2)
+	heap.Insert(3)
+	heap.Insert(highest)
+
+	peek, err := heap.Peek()
+
+	if err != nil {
+		t.Errorf("heap.Max() raised %s; expected no error", err.Error())
+	}
+	if peek != highest {
+		t.Errorf("heap.Max() == %d; expected %d", peek, highest)
+	}
+
+	nextPeek, err := heap.Peek()
+
+	if err != nil {
+		t.Errorf("heap.Max() raised %s; expected no error", err.Error())
+	}
+	if nextPeek != highest {
+		t.Errorf("heap.Max() == %d; expected %d", nextPeek, highest)
+	}
+}
+
+func TestParentFor(t *testing.T) {
+	cases := map[int]int{
+		1:  0,
+		2:  0,
+		3:  1,
+		4:  1,
+		5:  2,
+		6:  2,
+		7:  3,
+		8:  3,
+		9:  4,
+		10: 4,
+		11: 5,
+		12: 5,
+	}
+
+	for i, expected := range cases {
+		actual := parentFor(i)
+		if actual != expected {
+			t.Errorf("parentFor(%d) == %d; expected %d", i, actual, expected)
+		}
 	}
 }
